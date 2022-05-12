e9c767b1ac762adc055979199f8a0dc3594ebaac
vlpatterson59 <val@nutznboltz.website>
2022-05-12 14:26:54 -0400

    add some variable passing

--- app/src/main/java/gov/hocoboe/mibballotboxchainofcustody/Login.kt
+++ app/src/main/java/gov/hocoboe/mibballotboxchainofcustody/Login.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.navigation.findNavController
 
 class Login : Fragment() {
@@ -17,9 +18,12 @@ class Login : Fragment() {
         val view = inflater.inflate(R.layout.fragment_login, container, false)
         val loginButton = view.findViewById<Button>(R.id.login_register_login_button)
         val registerButton = view.findViewById<Button>(R.id.login_register_register_button)
+        val usernameView = view.findViewById<EditText>(R.id.login_username_editText)
 
         loginButton.setOnClickListener {
-            view.findNavController().navigate(R.id.action_login_to_transfer)
+            val loggedInAs = usernameView.text.toString()
+            val action = LoginDirections.actionLoginToTransfer(loggedInAs)
+            view.findNavController().navigate(action)
         }
 
         registerButton.setOnClickListener {
--- app/src/main/java/gov/hocoboe/mibballotboxchainofcustody/Transfer.kt
+++ app/src/main/java/gov/hocoboe/mibballotboxchainofcustody/Transfer.kt
@@ -1,14 +1,17 @@
 package gov.hocoboe.mibballotboxchainofcustody
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.findNavController
 
 class Transfer : Fragment() {
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -16,6 +19,9 @@ class Transfer : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_transfer, container, false)
 
+        val loggedInAs = TransferArgs.fromBundle(requireArguments()).loggedInAs
+        val loggedInAsView = view.findViewById<TextView>(R.id.transfer_logged_in_textView)
+        loggedInAsView.text = "Logged in as $loggedInAs"
         val retrieveButton = view.findViewById<Button>(R.id.transfer_retrieve_button)
         val transferButton = view.findViewById<Button>(R.id.transfer_transfer_button)
 
